822feb94255444190a52d529f45500c944c1a8e5
AngusWheatley <angus.wheatley2@education.nsw.gov.au>
2023-07-29 16:23:44 +1000

    Update 2  29/07/2023

--- SoftwareMajorProject/diaryEntryPage.Designer.cs
+++ SoftwareMajorProject/diaryEntryPage.Designer.cs
@@ -45,20 +45,18 @@
             // picBackPlate
             // 
             this.picBackPlate.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.picBackPlate.Location = new System.Drawing.Point(111, 10);
-            this.picBackPlate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picBackPlate.Location = new System.Drawing.Point(83, 8);
             this.picBackPlate.Name = "picBackPlate";
-            this.picBackPlate.Size = new System.Drawing.Size(1667, 985);
+            this.picBackPlate.Size = new System.Drawing.Size(1250, 800);
             this.picBackPlate.TabIndex = 8;
             this.picBackPlate.TabStop = false;
             // 
             // btnHome
             // 
             this.btnHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnHome.Location = new System.Drawing.Point(1784, 955);
-            this.btnHome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnHome.Location = new System.Drawing.Point(1338, 776);
             this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(107, 39);
+            this.btnHome.Size = new System.Drawing.Size(80, 32);
             this.btnHome.TabIndex = 25;
             this.btnHome.Text = "Home";
             this.btnHome.UseVisualStyleBackColor = true;
@@ -69,8 +67,7 @@
             this.lblEntryContents.AutoSize = true;
             this.lblEntryContents.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.lblEntryContents.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEntryContents.Location = new System.Drawing.Point(169, 294);
-            this.lblEntryContents.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEntryContents.Location = new System.Drawing.Point(127, 239);
             this.lblEntryContents.Name = "lblEntryContents";
             this.lblEntryContents.Size = new System.Drawing.Size(145, 26);
             this.lblEntryContents.TabIndex = 26;
@@ -78,20 +75,18 @@
             // 
             // TxtEntryContents
             // 
-            this.TxtEntryContents.Location = new System.Drawing.Point(176, 337);
-            this.TxtEntryContents.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtEntryContents.Location = new System.Drawing.Point(132, 274);
             this.TxtEntryContents.Multiline = true;
             this.TxtEntryContents.Name = "TxtEntryContents";
-            this.TxtEntryContents.Size = new System.Drawing.Size(736, 629);
+            this.TxtEntryContents.Size = new System.Drawing.Size(553, 512);
             this.TxtEntryContents.TabIndex = 27;
             // 
             // BtnDeleteEntry
             // 
             this.BtnDeleteEntry.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnDeleteEntry.Location = new System.Drawing.Point(1355, 436);
-            this.BtnDeleteEntry.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnDeleteEntry.Location = new System.Drawing.Point(1016, 354);
             this.BtnDeleteEntry.Name = "BtnDeleteEntry";
-            this.BtnDeleteEntry.Size = new System.Drawing.Size(364, 91);
+            this.BtnDeleteEntry.Size = new System.Drawing.Size(273, 74);
             this.BtnDeleteEntry.TabIndex = 33;
             this.BtnDeleteEntry.Text = "Delete";
             this.BtnDeleteEntry.UseVisualStyleBackColor = true;
@@ -99,10 +94,9 @@
             // BtnSaveEntry
             // 
             this.BtnSaveEntry.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnSaveEntry.Location = new System.Drawing.Point(971, 436);
-            this.BtnSaveEntry.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnSaveEntry.Location = new System.Drawing.Point(728, 354);
             this.BtnSaveEntry.Name = "BtnSaveEntry";
-            this.BtnSaveEntry.Size = new System.Drawing.Size(364, 91);
+            this.BtnSaveEntry.Size = new System.Drawing.Size(273, 74);
             this.BtnSaveEntry.TabIndex = 32;
             this.BtnSaveEntry.Text = "Save";
             this.BtnSaveEntry.UseVisualStyleBackColor = true;
@@ -110,11 +104,10 @@
             // 
             // TxtEntryTitle
             // 
-            this.TxtEntryTitle.Location = new System.Drawing.Point(176, 214);
-            this.TxtEntryTitle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtEntryTitle.Location = new System.Drawing.Point(132, 174);
             this.TxtEntryTitle.Multiline = true;
             this.TxtEntryTitle.Name = "TxtEntryTitle";
-            this.TxtEntryTitle.Size = new System.Drawing.Size(736, 50);
+            this.TxtEntryTitle.Size = new System.Drawing.Size(553, 41);
             this.TxtEntryTitle.TabIndex = 35;
             // 
             // lblEntryTitle
@@ -122,8 +115,7 @@
             this.lblEntryTitle.AutoSize = true;
             this.lblEntryTitle.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.lblEntryTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEntryTitle.Location = new System.Drawing.Point(169, 178);
-            this.lblEntryTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEntryTitle.Location = new System.Drawing.Point(127, 145);
             this.lblEntryTitle.Name = "lblEntryTitle";
             this.lblEntryTitle.Size = new System.Drawing.Size(215, 26);
             this.lblEntryTitle.TabIndex = 34;
@@ -131,10 +123,10 @@
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(271, 66);
-            this.txtUserName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtUserName.Location = new System.Drawing.Point(203, 54);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(152, 22);
+            this.txtUserName.Size = new System.Drawing.Size(115, 20);
             this.txtUserName.TabIndex = 36;
             // 
             // lblEntryDate
@@ -142,8 +134,7 @@
             this.lblEntryDate.AutoSize = true;
             this.lblEntryDate.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.lblEntryDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEntryDate.Location = new System.Drawing.Point(964, 178);
-            this.lblEntryDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEntryDate.Location = new System.Drawing.Point(723, 145);
             this.lblEntryDate.Name = "lblEntryDate";
             this.lblEntryDate.Size = new System.Drawing.Size(139, 26);
             this.lblEntryDate.TabIndex = 41;
@@ -151,8 +142,8 @@
             // 
             // CalEntryDate
             // 
-            this.CalEntryDate.Location = new System.Drawing.Point(971, 222);
-            this.CalEntryDate.Margin = new System.Windows.Forms.Padding(12, 11, 12, 11);
+            this.CalEntryDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalEntryDate.Location = new System.Drawing.Point(728, 180);
             this.CalEntryDate.MaxSelectionCount = 1;
             this.CalEntryDate.Name = "CalEntryDate";
             this.CalEntryDate.ShowTodayCircle = false;
@@ -161,10 +152,10 @@
             // 
             // DiaryEditorPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Highlight;
-            this.ClientSize = new System.Drawing.Size(1899, 1001);
+            this.ClientSize = new System.Drawing.Size(1424, 813);
             this.Controls.Add(this.lblEntryDate);
             this.Controls.Add(this.CalEntryDate);
             this.Controls.Add(this.txtUserName);
@@ -177,10 +168,9 @@
             this.Controls.Add(this.btnHome);
             this.Controls.Add(this.picBackPlate);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(1915, 1048);
-            this.MinimumSize = new System.Drawing.Size(1915, 1017);
+            this.MaximumSize = new System.Drawing.Size(1440, 859);
+            this.MinimumSize = new System.Drawing.Size(1440, 834);
             this.Name = "DiaryEditorPage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Noter - Diary Entry";
